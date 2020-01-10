8de421240047a4991bf99e12412012a860d256fa
Amanda Tree <amandasimonds9@gmail.com>
2020-01-09 18:30:40 -0700

    typos fixed in api routs and favorites api

--- routes/favorites-apiroutes.js
+++ routes/favorites-apiroutes.js
@@ -3,14 +3,14 @@ var db = require("../models");
 module.exports = function(app) {
 //route to get userID currently in use
     app.post("/api/userID", function(req, res) {
-        db.userFavorties.create(req.body).then(function(dbuserFavorites){
+        db.userFavorites.create(req.body).then(function(dbuserFavorites){
             res.json(dbuserFavorites);
         });
     });    
 
 //post create route for when a new favorite is added
     app.post("/api/myport", function(req, res) {
-        db.userFavorties.create(req.body).then(function(dbuserFavorites){
+        db.userFavorites.create(req.body).then(function(dbuserFavorites){
             res.json(dbuserFavorites);
         });
     });
@@ -28,12 +28,12 @@ module.exports = function(app) {
     });
 //join route with destroy on the userInfo routes so that favorites for this userID is deleted if the user deletes account.
     app.delete("/api/favorties/userID", function(req, res) {
-        db.userFavorties.destroy({
+        db.userFavorites.destroy({
             where: {
                 id: req.body.userID
             }
-        }).then(function(dbuserFavorties) {
-            res.json(dbuserFavorties);
+        }).then(function(dbuserFavorites) {
+            res.json(dbuserFavorites);
         });
     });
 }
